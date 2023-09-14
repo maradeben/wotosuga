3c747dad4a251d8d3d85b5e268f43795ed5f63fd
maradeben <maradebenezer@gmail.com>
2023-09-14 21:07:56 +0100

    minor fixes

--- backend/utils/result_generator.py
+++ backend/utils/result_generator.py
@@ -9,7 +9,8 @@ def generate_results(data):
     """ generate results and pass in dict form """
 
     name = data['Name']
-    diag = "Diabetic" if get_prediction(data) else "Non-diabetic"
+    diag = get_prediction(data)
+    diag_text = "Diabetic" if diag else "Non-diabetic"
     bmi = round(preprocess(data)['BMI'], 2)
     diag_plot = plots.plot_diag_by_bmi_sex(diag, data['Sex'], data['id'])[1]
     bmi_cat_plot = plots.plot_bmi_category(bmi, data['id'])[1]
@@ -18,7 +19,8 @@ def generate_results(data):
     results = {
         "name": name,
         "bmi": bmi,
-        "diagnosis": diag,
+        "raw_diag": diag,
+        "diagnosis": diag_text,
         "diagnosis_plot": diag_plot,
         "bmi_category_plot": bmi_cat_plot,
         "bmi_distribution_plot": bmi_dist_plot
